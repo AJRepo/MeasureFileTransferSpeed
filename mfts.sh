b60f7bcd1d8df9092d4a5855bfe3c0b3a1fe5469
AJRepo <AJRepo@users.noreply.github.com>
2023-07-19 13:27:46 -0500

    different stat versions

--- mfts.sh
+++ mfts.sh
@@ -12,27 +12,36 @@ DEST_FILE=$1
 ORIG_FILE=$2
 DEST="false"
 
+if stat -c %s "$0"; then
+  STAT_OPTS="-c %s"
+elif stat -f %z "$0"; then
+  STAT_OPTS="-f %z"
+else
+  echo "Error: cannot figure out your stat formatting"
+  exit 1
+fi
+
 if [ -f "$ORIG_FILE" ]; then
-	ORIG_SIZE=$(stat -c %s "$ORIG_FILE")
+	ORIG_SIZE=$(stat "$STAT_OPTS" "$ORIG_FILE")
 	DEST="true"
 else
 	DEST="false"
 fi
 SLEEP=1
-DEST_SIZE_1=$(stat -c %s "$DEST_FILE")
+DEST_SIZE_1=$(stat "$STAT_OPTS" "$DEST_FILE")
 DEST_SIZE_2SEC1=$DEST_SIZE_1
 for I in $(seq 1 10); do
   sleep $SLEEP
-  DEST_SIZE_2=$(stat -c %s "$DEST_FILE")
+  DEST_SIZE_2=$(stat "$STAT_OPTS" "$DEST_FILE")
   SPEED=$(echo "($DEST_SIZE_2 - $DEST_SIZE_1) / $SLEEP" | bc -l)
-  DEST_SIZE_1=$(stat -c %s "$DEST_FILE")
+  DEST_SIZE_1=$(stat "$STAT_OPTS" "$DEST_FILE")
 	SPEED_PER_K=$(echo "$SPEED/1000" | bc -l)
   echo "1 second averaged speed: $SPEED_PER_K kb/sec"
   if [ "$(echo "$I%2" | bc)" == 0 ]; then
     SPEED_2SEC=$(echo "($DEST_SIZE_2 - $DEST_SIZE_2SEC1) / (2*$SLEEP)" | bc -l)
 	  SPEED_PER_K=$(echo "$SPEED_2SEC/1000" | bc -l)
     echo "2 second averaged speed: $SPEED_PER_K kb/sec"
-    DEST_SIZE_2SEC1=$(stat -c %s "$DEST_FILE")
+    DEST_SIZE_2SEC1=$(stat "$STAT_OPTS" "$DEST_FILE")
   fi
 	if [[ $DEST == "true" ]]; then
     PERCENT_COMPLETE=$(echo "$DEST_SIZE_2/$ORIG_SIZE" | bc -l)
