79ed586dcc49eee7fc3de7bd8c791598cd9351d8
AJRepo <AJRepo@users.noreply.github.com>
2023-07-19 13:37:03 -0500

    refactor

--- mfts.sh
+++ mfts.sh
@@ -10,7 +10,7 @@ fi
 
 DEST_FILE=$1
 ORIG_FILE=$2
-DEST="false"
+DEST_ETA="false"
 
 if stat -c %s "$0" > /dev/null 2>&1; then
   STAT_OPTS="-c %s"
@@ -22,10 +22,10 @@ else
 fi
 
 if [ -f "$ORIG_FILE" ]; then
-	ORIG_SIZE=$(stat "$STAT_OPTS" "$ORIG_FILE")
-	DEST="true"
+  ORIG_SIZE=$(stat "$STAT_OPTS" "$ORIG_FILE")
+  DEST_ETA="true"
 else
-	DEST="false"
+  DEST_ETA="false"
 fi
 SLEEP=1
 DEST_SIZE_1=$(stat "$STAT_OPTS" "$DEST_FILE")
@@ -35,18 +35,18 @@ for I in $(seq 1 10); do
   DEST_SIZE_2=$(stat "$STAT_OPTS" "$DEST_FILE")
   SPEED=$(echo "($DEST_SIZE_2 - $DEST_SIZE_1) / $SLEEP" | bc -l)
   DEST_SIZE_1=$(stat "$STAT_OPTS" "$DEST_FILE")
-	SPEED_PER_K=$(echo "$SPEED/1000" | bc -l)
+  SPEED_PER_K=$(echo "$SPEED/1000" | bc -l)
   echo "1 second averaged speed: $SPEED_PER_K kb/sec"
   if [ "$(echo "$I%2" | bc)" == 0 ]; then
     SPEED_2SEC=$(echo "($DEST_SIZE_2 - $DEST_SIZE_2SEC1) / (2*$SLEEP)" | bc -l)
-	  SPEED_PER_K=$(echo "$SPEED_2SEC/1000" | bc -l)
+    SPEED_PER_K=$(echo "$SPEED_2SEC/1000" | bc -l)
     echo "2 second averaged speed: $SPEED_PER_K kb/sec"
     DEST_SIZE_2SEC1=$(stat "$STAT_OPTS" "$DEST_FILE")
   fi
-	if [[ $DEST == "true" ]]; then
+  if [[ $DEST_ETA == "true" ]]; then
     PERCENT_COMPLETE=$(echo "$DEST_SIZE_2/$ORIG_SIZE" | bc -l)
     ETA=$(echo "$ORIG_SIZE/$SPEED/60" | bc -l)
-	  echo "PERCENT: $PERCENT_COMPLETE %"
-	  echo "ETA: $ETA minutes"
-	fi
+    echo "PERCENT: $PERCENT_COMPLETE %"
+    echo "ETA: $ETA minutes"
+  fi
 done
